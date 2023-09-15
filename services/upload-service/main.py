e1316eb1a131e6465369fed6afd530ec4e7f3cb2
Ali Ok <aliok@redhat.com>
2023-09-15 11:42:24 +0300

    Upload service (#2)
    * Create a separate upload service - not working right now
    
    * Helm 3.8.0 is a prereq
    
    * Call upload instead of predict in the client
    
    * Pass max image w/h from env vars
    
    * Rename env vars
    
    * Fix success response to return JSON
    
    * Scale down image before uploading
    
    * Put everything under IIFE
    
    * Creating reusable functions out of crap
    
    * Update instructions to run upload service
    
    * Improve UI
    
    * Rename some method
    
    * Reject big requests before reading request body
    
    * Look like SV Hotdog app - home view
    
    * New bg in client
    
    * Faster image drawing on canvas
    
    * Custom loading modal
    
    * Success view
    
    * Failure view
    
    * UI fiddles
    
    * Made upload service to actually do an upload

---
+++ services/upload-service/main.py
@@ -0,0 +1,101 @@
+from flask import Flask, render_template, request
+from PIL import Image
+import base64
+import io
+import os
+import boto3
+import botocore
+import uuid
+
+MAX_IMG_SIZE_IN_BYTES = int(os.environ.get("MAX_IMG_SIZE_IN_BYTES", 1000 * 1000))
+MAX_IMG_WIDTH = int(os.environ.get("MAX_IMG_WIDTH", 640))
+MAX_IMG_HEIGHT = int(os.environ.get("MAX_IMG_HEIGHT", 640))
+
+S3_ENDPOINT_URL = os.environ.get("S3_ENDPOINT_URL")
+S3_ACCESS_KEY_ID = os.environ.get("S3_ACCESS_KEY_ID")
+S3_ACCESS_KEY_SECRET = os.environ.get("S3_ACCESS_KEY_SECRET")
+S3_ACCESS_SSL_VERIFY = bool(os.environ.get("S3_ACCESS_SSL_VERIFY", "true"))
+S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
+
+if not S3_ENDPOINT_URL:
+    raise Exception("Missing S3_ENDPOINT_URL")
+if not S3_ACCESS_KEY_ID:
+    raise Exception("Missing S3_ACCESS_KEY_ID")
+if not S3_ACCESS_KEY_SECRET:
+    raise Exception("Missing S3_ACCESS_KEY_SECRET")
+if not S3_BUCKET_NAME:
+    raise Exception("Missing S3_BUCKET_NAME")
+
+app = Flask(__name__)
+
+boto_config = botocore.client.Config(connect_timeout=5, retries={'max_attempts': 1})
+
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html
+s3 = boto3.client(
+    's3',
+    config=boto_config,
+    endpoint_url=S3_ENDPOINT_URL,
+    aws_access_key_id=S3_ACCESS_KEY_ID,
+    aws_secret_access_key=S3_ACCESS_KEY_SECRET,
+    verify=S3_ACCESS_SSL_VERIFY,
+)
+
+# check if the bucket exists
+try:
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/head_bucket.html
+    s3.head_bucket(Bucket=S3_BUCKET_NAME)
+except Exception as e:
+    print(e)
+    raise Exception(f"Bucket {S3_BUCKET_NAME} does not exist")
+
+@app.get('/')
+def send_client_html():
+    return render_template('index.html', max_img_width=MAX_IMG_WIDTH, max_img_height=MAX_IMG_HEIGHT)
+
+
+@app.post("/upload")
+def hello_world():
+    # set req size limit in Flask
+    # https://stackoverflow.com/questions/25036498/is-it-possible-to-limit-flask-post-data-size-on-a-per-route-basis
+    content_length = request.content_length
+    if content_length is None:
+        return "Missing content-length", 400
+
+    if content_length > MAX_IMG_SIZE_IN_BYTES:
+        return f"Request length too large {content_length / 1024:.2f} KB", 400
+
+    if "image_b64" not in request.json:
+        return "Missing image_b64", 400
+
+    image_base64 = request.json["image_b64"]
+
+    if len(image_base64) > MAX_IMG_SIZE_IN_BYTES:
+        return f"Image too large {len(image_base64) / 1024:.2f} KB", 400
+
+    try:
+        base64_decoded = base64.b64decode(image_base64)
+    except Exception as e:
+        print(e)
+        return f"Failed to decode image", 400
+
+    try:
+        image = Image.open(io.BytesIO(base64_decoded))
+    except Exception as e:
+        print(e)
+        return f"Failed to load the image", 400
+
+    width, height = image.size
+    if width > MAX_IMG_WIDTH or height > MAX_IMG_HEIGHT:
+        return f"Image too wide or tall {width}x{height}", 400
+
+    upload_id = uuid.uuid4().hex
+
+    try:
+        s3.put_object(Bucket=S3_BUCKET_NAME, Key=upload_id, Body=base64_decoded)
+    except Exception as e:
+        print(e)
+        return f"Failed to upload", 500
+
+    return {
+        "uploadId": upload_id
+    }, 200
