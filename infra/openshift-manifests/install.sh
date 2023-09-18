29199f1f7e91afdbe71dbf2c8f73315d5d9f2c9f
Pierangelo Di Pilato <pierdipi@redhat.com>
2023-09-18 10:05:49 +0200

    Wait CRD installation
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- infra/openshift-manifests/install.sh
+++ infra/openshift-manifests/install.sh
@@ -3,4 +3,8 @@
 current_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 manifests_dir="${current_dir}/../kind-manifests"
 
-kubectl apply -k "${current_dir}"
+while ! kubectl apply -k ${current_dir}
+do
+  echo "waiting for resource apply to succeed"
+  sleep 10
+done
