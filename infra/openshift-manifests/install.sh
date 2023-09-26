5d56a107ed85c5aa33f6a19a0ad19e4efbeb68a2
Pierangelo Di Pilato <pierdipi@redhat.com>
2023-09-26 16:35:51 +0200

    Deploy minio webhook source
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- infra/openshift-manifests/install.sh
+++ infra/openshift-manifests/install.sh
@@ -13,5 +13,16 @@ done
 # mc alias set ai-demo https://localhost:9445 minio minio1234 --insecure # set a minio host alias
 # kubectl port-forward -n minio-operator svc/minio-tenant-console 9444:9443 # port-forward tenant console (login with credentials in minio-storage-configuration.yaml)
 # mc mb ai-demo/ai-demo --insecure # Create bucket 'ai-demo'
-# mc event add ai-demo/ai-demo arn:minio:sqs::PRIMARY:webhook --event put --insecure #
-# mc admin config set ai-demo/ notify_webhook:PRIMARY endpoint="http://event-display.ai-demo.svc.cluster.local:80" --insecure
+
+# mc admin config set ai-demo/ notify_webhook:PRIMARY endpoint="http://event-display.ai-demo.svc.cluster.local:80" --insecure # Setup webhook notification to endpoint
+# mc admin service restart ai-demo/ --insecure # Required after a config change
+# mc event add ai-demo/ai-demo arn:minio:sqs::PRIMARY:webhook --event put --insecure # Subscribe to PUT events
+
+# mc admin config set ai-demo/ notify_webhook:minio-webhook-source endpoint="http://minio-webhook-source.ai-demo.svc.cluster.local:80" --insecure # Setup webhook notification to endpoint
+# mc admin service restart ai-demo/ --insecure # Required after a config change
+# mc event add ai-demo/ai-demo arn:minio:sqs::minio-webhook-source:webhook --event put --insecure # Subscribe to PUT events
+
+# Upload some files for testing
+# mc cp --recursive infra/openshift-manifests/ ai-demo/ai-demo --insecure
+
+ # mc rm --recursive ai-demo/ai-demo  --dangerous --force --insecure # VERY DANGEROUS: remove every file in bucket
