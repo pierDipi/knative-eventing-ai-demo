602ccfc5f6a84fb0550dfd5f39041e24953ed681
Pierangelo Di Pilato <pierdipi@redhat.com>
2023-09-26 11:34:36 +0200

    Update minio installation to deploy tenant
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- infra/openshift-manifests/install.sh
+++ infra/openshift-manifests/install.sh
@@ -8,3 +8,10 @@ do
   echo "waiting for resource apply to succeed"
   sleep 10
 done
+
+# kubectl port-forward -n minio-operator svc/minio 9445:443 # port-forward minio store
+# mc alias set ai-demo https://localhost:9445 minio minio1234 --insecure # set a minio host alias
+# kubectl port-forward -n minio-operator svc/minio-tenant-console 9444:9443 # port-forward tenant console (login with credentials in minio-storage-configuration.yaml)
+# mc mb ai-demo/ai-demo --insecure # Create bucket 'ai-demo'
+# mc event add ai-demo/ai-demo arn:minio:sqs::PRIMARY:webhook --event put --insecure #
+# mc admin config set ai-demo/ notify_webhook:PRIMARY endpoint="http://event-display.ai-demo.svc.cluster.local:80" --insecure
